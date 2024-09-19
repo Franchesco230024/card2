7a6b47594ace5cf0286a11ba70d529ab4326940b
Franchesco230024 <141521103+Franchesco230024@users.noreply.github.com>
2024-09-19 20:45:00 +0300

    Menu.java

--- Menu.java
+++ Menu.java
@@ -1,70 +1,49 @@
 import java.util.Scanner;
 
 public class Menu {
+    private final Scanner scanner = new Scanner(System.in); // Открываем Scanner один раз
+
     public void start() {
-
         Validator validator = new Validator();
-
         String cardNumber = getCardNumber();
 
         Card card = createCard(cardNumber, validator);
 
-        if (!isLengthValid(card, validator)) {
-            return;
+        if (validateCard(card, validator)) {
+            System.out.println("Карта валидна.");
+        } else {
+            System.out.println("Карта невалидна.");
         }
-
-        if (!getCardType(card)) {
-            return;
-        }
-
-        isLuhnValid(card, validator);
     }
 
     private String getCardNumber() {
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите номер карты: ");
-        String cardNumber = scanner.nextLine();
-        scanner.close();
-
-        return cardNumber;
+        return scanner.nextLine();
     }
 
     private Card createCard(String cardNumber, Validator validator) {
-
         return new Card(cardNumber, validator.getTypeOfCard(cardNumber));
     }
 
-    private boolean isLengthValid(Card card, Validator validator) {
-
-        if (!validator.isLengthValid(card.number)) {
+    private boolean validateCard(Card card, Validator validator) {
+        // Объединение всех проверок в одну функцию
+        if (!validator.isLengthValid(card.getNumber())) {
             System.out.println("Неверное количество цифр!");
             return false;
         }
 
-        return true;
-    }
-
-    private boolean getCardType(Card card) {
-        if (card.type == Type.INVALID) {
-            System.out.println("Неверный тип карты");
+        if (card.getType() == Type.INVALID) {
+            System.out.println("Неверный тип карты.");
             return false;
         } else {
-            System.out.println(card.type);
-            return true;
+            System.out.println("Тип карты: " + card.getType());
         }
-    }
 
-    private boolean isLuhnValid(Card card, Validator validator) {
-
-
-        if (validator.isLuhnValid(card.number)) {
-            System.out.println("Карта валидна");
-            return true;
-        }
-        else {
-            System.out.println("Карта невалидна");
+        if (!validator.isLuhnValid(card.getNumber())) {
+            System.out.println("Карта не прошла проверку по алгоритму Луна.");
             return false;
         }
+
+        return true;
     }
 }
